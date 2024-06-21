d8fa864297d8cfce65171cb0d4c94aafed16ee5c
Nehara831 <112460919+Nehara831@users.noreply.github.com>
2024-06-21 17:52:18 +0530

    Update WebConfig.java

--- VirtualFitON/src/main/java/com/example/VirtualFitON/Configurations/WebConfig.java
+++ VirtualFitON/src/main/java/com/example/VirtualFitON/Configurations/WebConfig.java
@@ -3,17 +3,15 @@ package com.example.VirtualFitON.Configurations;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
-
-
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","http://34.222.253.72")
-                .allowedOriginPatterns("*")
+                .allowedOrigins("http://localhost:3000", "http://34.222.253.72")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true); // Assuming you need this
     }
-
 }
