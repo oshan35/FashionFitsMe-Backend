3b6f536d0a43135fd6e25366af83aeb1c2aaecb7
ashen829 <ashennethsara99@gmail.com>
2024-06-24 01:34:09 +0530

    ddffs

--- VirtualFitON/src/main/java/com/example/VirtualFitON/ConfigLogger.java
+++ VirtualFitON/src/main/java/com/example/VirtualFitON/ConfigLogger.java
@@ -1,23 +1,23 @@
-//package com.example.VirtualFitON;
-//
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.event.ContextRefreshedEvent;
-//import org.springframework.context.event.EventListener;
-//import org.springframework.stereotype.Component;
-//
-//@Component
-//public class ConfigLogger {
-//
-//    @Value("${spring.data.redis.host}")
-//    private String redisHost;
-//
-//    @Value("${spring.data.redis.port}")
-//    private int redisPort;
-//
-//    @EventListener
-//    public void handleContextRefresh(ContextRefreshedEvent event) {
-//        System.out.println("Redis Host: " + redisHost);
-//        System.out.println("Redis Port: " + redisPort);
-//    }
-//}
-//
+package com.example.VirtualFitON;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConfigLogger {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    @EventListener
+    public void handleContextRefresh(ContextRefreshedEvent event) {
+        System.out.println("Redis Host: " + redisHost);
+        System.out.println("Redis Port: " + redisPort);
+    }
+}
+
